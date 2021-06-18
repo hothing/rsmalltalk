64cf1bdb4e5061e8335df778075a212b98bce118
Mykhayl Puzanov <barbos@inbox.ru>
2021-06-18 20:32:22 +0300

    Memory model redesign /5.3

--- memory-model/memory_test.adb
+++ memory-model/memory_test.adb
@@ -12,17 +12,39 @@ procedure Memory_Test is
 begin
 --   w := 1; -- it is valid integer, but not valid pointer
 --   putObjectField(mem, 1, w, 0, 0);
-   w := 30;
-   w0 := 1234;
-   putObjectField(mem, 1, w, integerObjectOf(0), w0);
-   w1 := getObjectField(mem, 1, w, integerObjectOf(0));
+--     w := 30;
+--     w0 := 1234;
+--     putObjectField(mem, 1, w, integerObjectOf(0), w0);
+--     w1 := getObjectField(mem, 1, w, integerObjectOf(0));
+--     pragma Assert(w0 = w1);
+--     TIO.Put_Line(T_Word'Image(w1));
+--
+--     w := T_Word'Last;
+--     w0 := 5678;
+--     putObjectField(mem, 1, w, integerObjectOf(10), w0);
+--     w1 := getObjectField(mem, 1, w, integerObjectOf(10));
+--     pragma Assert(w0 = w1);
+--     TIO.Put_Line(T_Word'Image(w1));
+
+   w := 40;
+   w0 := integerObjectOf(10);
+   putObjectSize(mem, 1, w, w0);
+   w1 := getObjectSize(mem, 1, w);
    pragma Assert(w0 = w1);
    TIO.Put_Line(T_Word'Image(w1));
 
-   w := T_Word'Last;
+   w := 40;
    w0 := 5678;
-   putObjectField(mem, 1, w, integerObjectOf(10), w0);
-   w1 := getObjectField(mem, 1, w, integerObjectOf(10));
+   putObjectSize(mem, 1, w, integerObjectOf(10));
+   w1 := getObjectField(mem, 1, w, integerObjectOf(0));
+   pragma Assert(w0 /= w1);
+   TIO.Put_Line(T_Word'Image(w1));
+
+   w := 40;
+   w0 := integerObjectOf(10);
+   putObjectClass(mem, 1, w, w0);
+   w1 := getObjectClass(mem, 1, w);
    pragma Assert(w0 = w1);
    TIO.Put_Line(T_Word'Image(w1));
+
 end Memory_Test;
